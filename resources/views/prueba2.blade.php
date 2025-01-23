5112ec7daf37d639aa3a7495922ae6531e0be075
Deyber Manrique <dbrmanrique@gmail.com>
2025-01-23 00:32:01 -0500

    Haciendo pruebas

--- resources/views/prueba2.blade.php
+++ resources/views/prueba2.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prueba 2</title>
+    <title>Prueba 2 asdfasdf</title>
 </head>
 <body>
-    <h1>Prueba 2</h1>
+    <h1>Prueba 2asdfasd</h1>
+    <p>agregando un párrafo</p>
 </body>
 </html>
