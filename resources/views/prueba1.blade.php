d1b3e8b63ccbbcef0584e1acc78b95853048a758
Ever-PR <ever.g.pr2019@gmail.com>
2025-01-23 00:30:30 -0500

    Cambios 1 vistas de prueba

--- resources/views/prueba1.blade.php
+++ resources/views/prueba1.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prueba 1</title>
+    <title>Prueba 1 - 1</title>
 </head>
 <body>
-    <h1>Prueba 1</h1>
+    <h1>Prueba 1 - 1</h1>
 </body>
 </html>
--- resources/views/prueba2.blade.php
+++ resources/views/prueba2.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prueba 2</title>
+    <title>Prueba 2 - 2</title>
 </head>
 <body>
-    <h1>Prueba 2</h1>
+    <h1>Prueba 2 - 2</h1>
 </body>
 </html>
