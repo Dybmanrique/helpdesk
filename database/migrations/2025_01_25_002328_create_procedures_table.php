430615187bb5776c778c066bfc2e1b8ddd405634
Deyber Manrique <dbrmanrique@gmail.com>
2025-01-24 20:03:57 -0500

    adding migrations

---
+++ database/migrations/2025_01_25_002328_create_procedures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('procedures', function (Blueprint $table) {
+            $table->id();
+            $table->string('reason');
+            $table->text('description');
+            $table->string('ticket');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('procedure_priority_id')->constrained();
+            $table->foreignId('procedure_category_id')->constrained();
+            $table->foreignId('procedure_state_id')->constrained();
+            $table->foreignId('document_type_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('procedures');
+    }
+};
