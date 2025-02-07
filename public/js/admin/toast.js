b327a305387965aa5bf761f817c58e73a2a9eede
Deyber <dbrmanrique@gmail.com>
2025-02-06 19:14:57 -0500

    feat: adding js component for admin alerts

---
+++ public/js/admin/toast.js
@@ -0,0 +1,36 @@
+var Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+
+document.addEventListener('livewire:initialized', () => {
+    Livewire.on('notify', (eventData) => {
+        const data = eventData[0];
+        if (data.code == 200) {
+            Toast.fire({
+                icon: 'success',
+                title: data.message
+            });
+        } else if (data.code == 500) {
+            Toast.fire({
+                icon: 'info',
+                title: data.message
+            });
+        }
+    });
+});
+
+function confirmationMessage(title = "¿Está seguro?", text = "Esta operación es irreversible", icon = "warning") {
+    return Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33", 
+        confirmButtonText: "SÍ, ELIMINAR!",
+        cancelButtonText: "CANCELAR"
+    }).then((result) => result.isConfirmed);
+ }
