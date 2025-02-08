cd5f6121526227ce9bdeaa5dad7f54441f659f72
Deyber Manrique <dbrmanrique@gmail.com>
2025-02-07 22:38:26 -0500

    style: positioning the modal in the center

--- resources/views/livewire/admin/document-types/crud.blade.php
+++ resources/views/livewire/admin/document-types/crud.blade.php
@@ -2,7 +2,7 @@
     <!-- Modal -->
     <div wire:ignore.self class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalTitle"
         aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <form wire:submit='save'>
                     <div class="modal-header">
@@ -29,7 +29,7 @@
                         </div>
                         <div class="mb-3" wire:loading.remove wire:target.except="save">
                             <label for="name" class="form-label">Nombre (*):</label>
-                            <input type="text" class="form-control" wire:model='name' id="name">
+                            <input type="text" class="form-control" wire:model='name' id="name" required autocomplete="off">
                             @error('name')
                                 <div class="form-text text-danger">{{ $message }}</div>
                             @enderror
