915d73ff05f0cbd17d0103e3dbc3212812f1b42e
Ever-PR <ever.g.pr2019@gmail.com>
2025-03-17 12:17:57 -0500

    fix: remove migration procedure_user and add foreign key action_id in action_files

--- database/migrations/2025_03_17_021914_create_action_files_table.php
+++ database/migrations/2025_03_17_021914_create_action_files_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('path');
+            $table->foreignId('action_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
