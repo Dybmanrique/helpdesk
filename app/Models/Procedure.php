42042a71829a5a9c98b5ac2768e4817265ec5d86
Deyber Manrique <dbrmanrique@gmail.com>
2025-03-17 21:35:28 -0500

    fix: correcting relationships

--- app/Models/Procedure.php
+++ app/Models/Procedure.php
@@ -19,33 +19,42 @@ class Procedure extends Model
         'document_type_id',
         'user_id',
     ];
-    
-    public function document_type(){
-        return $this->belongsTo(DocumentType::class,'document_type_id','id');
+
+    public function document_type()
+    {
+        return $this->belongsTo(DocumentType::class, 'document_type_id', 'id');
     }
-    public function procedure_category(){
-        return $this->belongsTo(ProcedureCategory::class,'procedure_category_id','id');
+    public function procedure_category()
+    {
+        return $this->belongsTo(ProcedureCategory::class, 'procedure_category_id', 'id');
     }
-    public function procedure_priority(){
-        return $this->belongsTo(ProcedurePriority::class,'procedure_priority_id','id');
+    public function procedure_priority()
+    {
+        return $this->belongsTo(ProcedurePriority::class, 'procedure_priority_id', 'id');
     }
-    public function procedure_state(){
-        return $this->belongsTo(ProcedureState::class,'procedure_state_id','id');
+    public function procedure_state()
+    {
+        return $this->belongsTo(ProcedureState::class, 'procedure_state_id', 'id');
     }
-    public function files(){
-        return $this->hasMany(File::class,'procedure_id','id');
+    public function files()
+    {
+        return $this->hasMany(File::class, 'procedure_id', 'id');
     }
-    public function derivations(){
-        return $this->hasMany(Derivation::class,'procedure_id','id');
+    public function derivations()
+    {
+        return $this->hasMany(Derivation::class, 'procedure_id', 'id');
     }
-    public function comments(){
-        return $this->hasMany(Comment::class,'procedure_id','id');
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'procedure_id', 'id');
     }
-    public function people(){
-        return $this->belongsToMany(Person::class,'procedure_person','procedure_id','person_id');
+    public function people()
+    {
+        return $this->belongsToMany(Person::class, 'procedure_person', 'procedure_id', 'person_id');
     }
-    public function users(){
-        return $this->belongsToMany(User::class,'for_knowledge','procedure_id','user_id');
+    public function users_for_knowledge()
+    {
+        return $this->belongsToMany(User::class, 'for_knowledge', 'procedure_id', 'user_id');
     }
     public function user()
     {
