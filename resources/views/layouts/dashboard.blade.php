9544e4b6013199d42200b41d8c7ad6f161fe82e6
Deyber Manrique <dbrmanrique@gmail.com>
2025-04-12 21:39:11 -0500

    Refactor: Adding csrf code to the header

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="description" content="CoreUI - Open Source Bootstrap Admin Template">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <meta name="theme-color" content="#ffffff">
     <!-- Vendors styles-->
