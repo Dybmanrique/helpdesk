866d7f6e73983ad2e5855d74c02039cf96f1c178
Deyber Manrique <dbrmanrique@gmail.com>
2025-05-09 11:07:40 -0500

    fix: add new atributes

--- app/Models/AdministrativeUser.php
+++ app/Models/AdministrativeUser.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class AdministrativeUser extends Model
 {
     protected $table = "administrative_users";
-    protected $fillable = ['user_id','office_id'];
+    protected $fillable = ['user_id','office_id', 'is_default'];
 
 }
