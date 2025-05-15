f8fafcabd2efe97cd833bd83f5927308c89819b0
Deyber Manrique <dbrmanrique@gmail.com>
2025-05-15 13:20:50 -0500

    fix: add id to administrative_users table

--- database/migrations/2025_01_25_003733_create_administrative_users_table.php
+++ database/migrations/2025_01_25_003733_create_administrative_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('administrative_users', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('office_id')->constrained();
             $table->boolean('is_default')->default(false);
