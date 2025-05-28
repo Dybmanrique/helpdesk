c42d3ed6efeeee7abb9f442e0fe0a4a8f8b529ed
Ever-PR <ever.g.pr2019@gmail.com>
2025-05-27 21:53:35 -0500

    feat(models): add the setting to have Year as the custom primary key in ProcedureCounter model

--- app/Models/ProcedureCounter.php
+++ app/Models/ProcedureCounter.php
@@ -8,4 +8,7 @@ class ProcedureCounter extends Model
 {
     protected $table = "procedure_counters";
     protected $fillable = ['year', 'last_number'];
+    protected $primaryKey = 'year';
+    public $incrementing = false;
+    protected $keyType = 'int';
 }
